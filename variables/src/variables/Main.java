79113c0d321548bc1c95be5f607f5cce9aff8d95
Galip <Galip@DESKTOP-HRK9HK1>
2021-05-01 02:59:58 +0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int ogrenciSayi=9;
+		String mesaj="Öðrenci Sayýsý : ";
+
+		System.out.println("Öðrenci Sayýsý : "+ ogrenciSayi);
+		System.out.println("Öðrenci Sayýsý : 9");
+		System.out.println("Öðrenci Sayýsý : 9");
+		System.out.println(mesaj+ogrenciSayi);
+		
+	}
+
+}
