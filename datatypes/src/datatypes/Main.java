3d40c0a3d4b645256619ced336e06f3eb91e5359
Galip <Galip@DESKTOP-HRK9HK1>
2021-05-01 02:56:07 +0300

    datatypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//case sensitive o yüzden büyük küçük harfe dikkat et
+		int sayi=12;
+		String sehir="Ankara";
+		char karakter='A';
+	}
+
+}
