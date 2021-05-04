318d324e58eaf7e422ed121de4cf6088e8a6a591
Galip <Galip@DESKTOP-HRK9HK1>
2021-05-04 18:06:22 +0300

    abstract demo

---
+++ abstractDemo/src/abstractDemo/MySqlDatabaeManager.java
@@ -0,0 +1,11 @@
+package abstractDemo;
+
+public class MySqlDatabaeManager extends BaseDatabaseManager{
+
+	@Override
+	public void getData() {
+		System.out.println("Vari getirildi : MySql");
+		
+	}
+
+}
